96251a81f945fe3d54169f9925cb2798344132fd
palmdrop <anton@exlex.se>
2021-02-02 09:23:54 +0100

    add comments and fix bounds function

--- Assets/Scripts/Map/MapScript.cs
+++ Assets/Scripts/Map/MapScript.cs
@@ -4,31 +4,40 @@ namespace Map
 {
     public class MapScript : MonoBehaviour
     {
+        // Children of the map object
         private GameObject _ground;        
         private GameObject _walls;        
         private GameObject _corners;
 
+        // The dimensions of the map. Is calculated using the positions of the corners.
         private Rect _bounds;
         
         void Start()
         {
+            // Get children
             _ground  = transform.GetChild(0).gameObject;
             _walls   = transform.GetChild(1).gameObject;
             _corners = transform.GetChild(2).gameObject;
 
+            // Get some of the corners in order to calculate dimensions
             Component cornerNW = _corners.transform.GetChild(0);
             Component cornerNE = _corners.transform.GetChild(1);
             Component cornerSW = _corners.transform.GetChild(2);
             
             _bounds = new Rect(
+                // Upper left corner is same as the position of the north west corner
                 cornerNW.transform.localPosition,
+                // Width and height is calculated using the difference in x and z 
                 new Vector2(
                     cornerNE.transform.localPosition.x - cornerNW.transform.localPosition.x,
-                    cornerSW.transform.localPosition.y - cornerNW.transform.localPosition.y
+                    cornerSW.transform.localPosition.z - cornerNW.transform.localPosition.z
                 )
             );
+        }
 
-            Debug.Log(_bounds);
+        public Rect GetBounds()
+        {
+            return _bounds;
         }
     }
 }
