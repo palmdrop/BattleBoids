d234109fafd8909811bda1aa0ecb063b2092eb25
Johannes Kvernes <johannes.kvernes@gmail.com>
2021-02-02 15:41:44 +0100

    Create boid spawner

---
+++ Assets/Scripts/Boid/BoidSpawner.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoidSpawner : MonoBehaviour
+{
+
+    public Boid boidPrefab;
+    public float spawnRadius = 1f;
+    public int spawnCount = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < spawnCount; i++)
+        {
+            Vector2 randomPos = Random.insideUnitCircle;
+            Vector3 pos = transform.position + new Vector3(randomPos.x, 0, randomPos.y) * spawnRadius;
+            Boid boid = Instantiate(boidPrefab);
+            boid.transform.position = pos;
+            Vector2 randomHeading = Random.insideUnitCircle;
+            boid.transform.forward = new Vector3(randomHeading.x, 0, randomHeading.y);
+        }
+    }
+
+}
