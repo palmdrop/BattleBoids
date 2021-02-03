fef0286234fe3c41c0266ea0760c7d8447f1d8e1
palmdrop <anton@exlex.se>
2021-02-03 13:54:14 +0100

    basic tile test

--- Assets/Tests/TileTestScript.cs
+++ Assets/Tests/TileTestScript.cs
@@ -3,24 +3,28 @@ using System.Collections.Generic;
 using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
+using Map;
+using UnityEditor;
 
 public class TileTestScript
 {
-    // A Test behaves as an ordinary method
-    [Test]
-    public void TileTestScriptSimplePasses()
-    {
-        // Use the Assert class to test conditions
-    }
-
     // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
     // `yield return null;` to skip a frame.
     [UnityTest]
     public IEnumerator TileTestScriptWithEnumeratorPasses()
     {
-        // Use the Assert class to test conditions.
-        // Use yield to skip a frame.
-        Assert.True(true);
+        //GameObject tileGameObject = MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/Tile"));
+        //TileScript tile = tileGameObject.GetComponent<TileScript>();
+        var asset = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/Prefabs/Tile.prefab");
+        GameObject tile = GameObject.Instantiate(asset, new Vector3(0, 0, 0), Quaternion.identity);
+
+        float sum = 0.0f;
+        foreach (Transform child in tile.transform)
+        {
+            sum += child.localScale.y;
+        }
+        
+        Assert.AreEqual(1.0, sum, 0.001);
         yield return null;
     }
 }
