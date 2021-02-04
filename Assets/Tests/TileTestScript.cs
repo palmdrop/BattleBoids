ad1bf2064584962abc46f8d283a8a40486e2646c
palmdrop <anton@exlex.se>
2021-02-04 15:12:55 +0100

    rename tile test and add scene to build

--- Assets/Tests/TileTestScript.cs
+++ Assets/Tests/TileTestScript.cs
@@ -3,28 +3,29 @@ using System.Collections.Generic;
 using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
-using Map;
 using UnityEditor;
 
 public class TileTestScript
 {
-    // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
-    // `yield return null;` to skip a frame.
     [UnityTest]
-    public IEnumerator TileTestScriptWithEnumeratorPasses()
+    public IEnumerator TestIfSumOfChildrenYScaleIsOne()
     {
-        //GameObject tileGameObject = MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/Tile"));
-        //TileScript tile = tileGameObject.GetComponent<TileScript>();
+        // Load Tile prefab
         var asset = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/Prefabs/Tile.prefab");
+        // Instantiate a tile
         GameObject tile = GameObject.Instantiate(asset, new Vector3(0, 0, 0), Quaternion.identity);
+        
+        // Wait for the end of the frame (this makes sure the tile "Start" method is called)
+        yield return new WaitForEndOfFrame();
 
+        // The sum of the y-scaling of each child transform should be 1.0
         float sum = 0.0f;
         foreach (Transform child in tile.transform)
         {
             sum += child.localScale.y;
         }
         
+        // Allow a small delta, in the case of rounding errors
         Assert.AreEqual(1.0, sum, 0.001);
-        yield return null;
     }
 }
