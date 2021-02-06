ee3b01d54d6c7c5ebc05eeca20ca5adc0e5e328c
palmdrop <anton@exlex.se>
2021-02-06 15:02:04 +0100

    avoid unnecessary component fetches and calls to manager

--- Assets/Scripts/Boid/Boid.cs
+++ Assets/Scripts/Boid/Boid.cs
@@ -5,54 +5,58 @@ using UnityEngine;
 public class Boid : MonoBehaviour
 {
 
-    private BoidManager manager;
     [SerializeField] private float viewRadius = 5f;
     [SerializeField] private float separationRadius = 2f;
     [SerializeField] private float alignmentStrength = 0.1f;
     [SerializeField] private float cohesionStrength = 0.2f;
     [SerializeField] private float separationStrength = 3f;
+    
+    private BoidManager _manager;
+    private Rigidbody _rigidbody;
 
     // Start is called before the first frame update
     void Start()
     {
-        manager = GameObject.FindGameObjectWithTag("BoidManager").GetComponent<BoidManager>();
+        _manager = GameObject.FindGameObjectWithTag("BoidManager").GetComponent<BoidManager>();
+        _rigidbody = GetComponent<Rigidbody>();
     }
 
     // Called by the boid manager
     // Updates the boid according to the standard flocking behaviour
     public void UpdateBoid()
     {
-        Vector3 alignmentForce = alignment() * alignmentStrength;
-        Vector3 cohesionForce = cohesion() * cohesionStrength;
-        Vector3 separationForce = separation() * separationStrength;
+        Boid[] neighbours = _manager.findBoidsWithinRadius(this, viewRadius);
+        
+        Vector3 alignmentForce = alignment(neighbours) * alignmentStrength;
+        Vector3 cohesionForce = cohesion(neighbours) * cohesionStrength;
+
+        neighbours = _manager.findBoidsWithinRadius(this, separationRadius);
+        Vector3 separationForce = separation(neighbours) * separationStrength;
 
         Vector3 force = alignmentForce + cohesionForce + separationForce;
 
-        Rigidbody rb = GetComponent<Rigidbody>();
-        rb.AddForce(force, ForceMode.Acceleration);
+        _rigidbody.AddForce(force, ForceMode.Acceleration);
 
-        transform.forward = rb.velocity;
+        transform.forward = _rigidbody.velocity;
     }
 
     // Returns the position of this boid
     public Vector3 getPos()
     {
-        return GetComponent<Rigidbody>().position;
+        return _rigidbody.position;
     }
 
     // Returns the velocity of this boid
     public Vector3 getVel()
     {
-        return GetComponent<Rigidbody>().velocity;
+        return _rigidbody.velocity;
     }
 
     // Returns a direction vector for the alignment part of the flocking behaviour
-    private Vector3 alignment()
+    private Vector3 alignment(Boid[] neighbours)
     {
         Vector3 avgVel = new Vector3(0, 0, 0);
         int n = 0;
-
-        Boid[] neighbours = manager.findBoidsWithinRadius(this, viewRadius);
 
         foreach (Boid b in neighbours)
         {
@@ -65,12 +69,10 @@ public class Boid : MonoBehaviour
     }
 
     // Returns a direction vector for the cohesion part of the flocking behaviour
-    private Vector3 cohesion()
+    private Vector3 cohesion(Boid[] neighbours)
     {
         Vector3 avgPos = new Vector3(0, 0, 0);
         int n = 0;
-
-        Boid[] neighbours = manager.findBoidsWithinRadius(this, viewRadius);
 
         foreach (Boid b in neighbours)
         {
@@ -83,12 +85,10 @@ public class Boid : MonoBehaviour
     }
 
     // Returns a direction vector for the separation part of the flocking behaviour
-    private Vector3 separation()
+    private Vector3 separation(Boid[] neighbours)
     {
         Vector3 avgPos = new Vector3(0, 0, 0);
         int n = 0;
-
-        Boid[] neighbours = manager.findBoidsWithinRadius(this, separationRadius);
 
         foreach (Boid b in neighbours)
         {
