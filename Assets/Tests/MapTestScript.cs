ea93a5f4a36d0323825de13db4d70833cd6771f4
palmdrop <anton@exlex.se>
2021-02-09 10:55:39 +0100

    fix heightmap errors and add tests

--- Assets/Tests/MapTestScript.cs
+++ Assets/Tests/MapTestScript.cs
@@ -120,4 +120,102 @@ public class MapTestScript
         // If a tile coincides with each bound 
         Assert.True(topEdge && leftEdge && bottomEdge && rightEdge);
     }
+
+    [UnityTest]
+    public IEnumerator TestIfGroundTileHeightCorrespondsToHeightmapValue()
+    {
+        // Find map, map script and ground component
+        GameObject map = GameObject.Find("Map");
+        yield return new WaitForEndOfFrame();
+        MapScript mapScript = map.GetComponent<MapScript>();
+        Component ground = map.transform.Find("Ground");
+
+        // Iterate over all children
+        for (int i = 0; i < ground.transform.childCount; i++)
+        {
+            Transform child = ground.transform.GetChild(i);
+            
+            // Calculate the height of the child
+            float tileHeight = child.position.y + child.localScale.y / 2.0f;
+
+            // Iterate over all corners of the children (and the center), to make sure the heightmap has the correct
+            // value for every point on the child.
+            for (float dx = -0.50f; dx <= 0.50f; dx += 0.50f)
+            {
+                for (float dy = -0.50f; dy <= 0.50; dy += 0.50f)
+                {
+                    float heightmapValue = mapScript.HeightmapLookup(child.localPosition 
+                                                                    // scale the offset slightly to avoid problems where the tiles are adjacent
+                                                                     + new Vector3(dx, 0.0f, dy) * 0.999f); 
+                    
+                    // allow a small delta to account for precision errors 
+                    if (Mathf.Abs(heightmapValue - tileHeight) > 0.000001)
+                    {
+                        Assert.Fail();
+                    }
+                }
+            }
+        }
+
+        Assert.Pass();
+    }
+
+    [UnityTest]
+    public IEnumerator TestIfEveryTileIsAtCorrectGridPosition()
+    {
+        // Find map, mapScript and ground
+        GameObject map = GameObject.Find("Map");
+        yield return new WaitForEndOfFrame();
+        MapScript mapScript = map.GetComponent<MapScript>();
+        Component ground = map.transform.Find("Ground");
+
+        // Iterate over all children 
+        for (int i = 0; i < ground.transform.childCount; i++)
+        {
+            GameObject child = ground.transform.GetChild(i).gameObject;
+
+            // Make sure the child is the same as the tile saved in the internal map grid at that position
+            if (child != mapScript.GetGroundTileAt(child.transform.localPosition))
+            {
+                Assert.Fail();
+            }
+        }
+
+        Assert.Pass();
+    }
+
+    [UnityTest]
+    public IEnumerator TestIfEveryNullTileHasHeightmapValueOfMinValue()
+    {
+        // Find map, mapScript and ground
+        GameObject map = GameObject.Find("Map");
+        yield return new WaitForEndOfFrame();
+        MapScript mapScript = map.GetComponent<MapScript>();
+        Rect bounds = mapScript.GetBounds();
+        
+        // Iterate over all discrete bound positions 
+        for (float x = bounds.x; x <= bounds.x + bounds.width; x += 1.0f)
+        {
+            for (float z = bounds.y; z <= bounds.y + bounds.height; z += 1.0f)
+            {
+                Vector3 position = new Vector3(x, 0.0f, z);
+
+                GameObject tile = mapScript.GetGroundTileAt(position);
+                
+                // Check if the tile at the corresponding position is null
+                if (tile == null)
+                {
+                    // If null, make sure the heightmap value is float.MinValue
+                    float heightmapValue = mapScript.HeightmapLookup(position);
+                    if (heightmapValue != float.MinValue)
+                    {
+                        Assert.Fail();
+                    }
+                }
+            }
+        }
+
+        Assert.Pass();
+    }
+    
 }
