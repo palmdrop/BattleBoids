a8506edf61a9b1870bf1ea990e78b357abaa3c3e
palmdrop <anton@exlex.se>
2021-02-09 10:55:39 +0100

    fix heightmap errors and add tests

--- Assets/Scripts/Map/MapScript.cs
+++ Assets/Scripts/Map/MapScript.cs
@@ -13,9 +13,12 @@ namespace Map
         private Rect _bounds;
         
         // A heightmap representation of the map. The heightmap only contains values for the tiles in "Ground"
-        private int _heightMapWidth;
-        private int _heightMapHeight;
+        private int _heightmapWidth;
+        private int _heightmapHeight;
         private float[] _heightmap;
+        
+        // The list of all child tiles, placed in a grid corresponding to the heightmap
+        private GameObject[] _groundTiles;
         
         void Start()
         {
@@ -26,7 +29,7 @@ namespace Map
             CalculateBounds();
             
             // Calculate the heightmap representation of the ground of the map 
-            CalculateHeightMap();
+            CalculateHeightmap();
         }
         
         private void CalculateBounds()
@@ -60,23 +63,27 @@ namespace Map
             );
         }
 
-        private int PositionToIndex(Vector3 point)
+        // Transforms a 3d point into an index. Only uses the x and z component.
+        private int PointToIndex(Vector3 point)
         {
-            int x = (int) (point.x - _bounds.x - 0.5);
-            int y = (int) (point.z - _bounds.y - 0.5);
-            return x + y * _heightMapWidth;
+            int x = (int) (point.x - _bounds.x + 0.5);
+            int y = (int) (point.z - _bounds.y + 0.5);
+            return x + y * _heightmapWidth;
         }
         
-        private void CalculateHeightMap() 
+        private void CalculateHeightmap() 
         {
             // Since the map is tiled based, the heightmap doesn't need any more detail 
             // than the number of tiles direction 
             // NOTE: this assumes that each tile is of unit size and none overlap
-            _heightMapWidth = Mathf.CeilToInt(_bounds.width) + 1;
-            _heightMapHeight = Mathf.CeilToInt(_bounds.height) + 1;
+            _heightmapWidth = Mathf.CeilToInt(_bounds.width) + 1;
+            _heightmapHeight = Mathf.CeilToInt(_bounds.height) + 1;
             
             // Instantiate heightmap
-            _heightmap = new float[_heightMapWidth * _heightMapHeight];
+            _heightmap = new float[_heightmapWidth * _heightmapHeight];
+            
+            // Instantiate tile grid
+            _groundTiles = new GameObject[_heightmap.Length];
 
             for (int i = 0; i < _heightmap.Length; i++) _heightmap[i] = float.MinValue;
             
@@ -86,22 +93,21 @@ namespace Map
                 GameObject child = _ground.transform.GetChild(i).gameObject;
                 
                 // Calculate height and assign value to heightmap index
-                float y = child.transform.localPosition.y + child.transform.localScale.y / 2.0f;
+                Vector3 childPosition = child.transform.localPosition;
+                float y = childPosition.y + child.transform.localScale.y / 2.0f;
                 
-                _heightmap[PositionToIndex(child.transform.position)] = y;
+                int index = PointToIndex(childPosition);
+                _heightmap[index] = y;
+                
+                // add tile to grid
+                _groundTiles[index] = child;
             }
         }
-        // Calculates if a given point is inside the map bounds
-        public bool PointInsideBounds(Vector3 point)
-        {
-            return point.x > _bounds.x && point.x < _bounds.x + _bounds.width 
-                && point.z > _bounds.y && point.z < _bounds.y + _bounds.height;
-        }
 
-        public float HeightmapLookup(Vector3 position)
+        public float HeightmapLookup(Vector3 point)
         {
             // Get the corresponding index of the position
-            int index = PositionToIndex(position);
+            int index = PointToIndex(point);
             
             // If the index is outside the range of the heightmap, this means
             // the position is outside the bounds of the map. Assume there's no ground at this position, and return 
@@ -111,7 +117,22 @@ namespace Map
             // Otherwise, if valid index, return corresponding heightmap value
             return _heightmap[index];
         }
+        
+        // Calculates if a given point is inside the map bounds
+        public bool PointInsideBounds(Vector3 point)
+        {
+            return point.x >= _bounds.x && point.x <= _bounds.x + _bounds.width 
+                && point.z >= _bounds.y && point.z <= _bounds.y + _bounds.height;
+        }
 
+        // Returns the ground tile at a specified position. Returns null if outside bounds or if there's no tile at 
+        // the specified position.
+        public GameObject GetGroundTileAt(Vector3 point)
+        {
+            if (PointInsideBounds(point)) return _groundTiles[PointToIndex(point)];
+            return null;
+        }
+        
         public Rect GetBounds()
         {
             return _bounds;
