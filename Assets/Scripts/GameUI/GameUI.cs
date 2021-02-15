d066be7a68d7939a38ebfe9878f3c349c086830d
Anton Ahl <ahanton@student.chalmers.se>
2021-02-15 16:09:18 +0100

    Add player selection to the UI

--- Assets/Scripts/GameUI/GameUI.cs
+++ Assets/Scripts/GameUI/GameUI.cs
@@ -5,23 +5,57 @@ using UnityEngine.UI;
 
 public class GameUI : MonoBehaviour
 {
-    [SerializeField] private Player player;
+    [SerializeField] private List<Player> players;
     [SerializeField] private Text boins;
+    [SerializeField] private Dropdown playerSelect;
+    [SerializeField] private Player activePlayer;
 
     // Start is called before the first frame update
     void Start()
     {
+        InitPlayerDropdown();
+        activePlayer = SetActivePlayer();
         UpdateBoins();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        activePlayer = SetActivePlayer();
+        UpdateBoins();
+    }
+
+    void InitPlayerDropdown()
+    {
+        playerSelect.ClearOptions();
+        foreach (var player in players)
+        {
+            Dropdown.OptionData newPlayer = new Dropdown.OptionData();
+            newPlayer.text = player.GetNickname();
+            playerSelect.options.Add(newPlayer);
+        }
+    }
+
+    Player SetActivePlayer()
+    {
+        string nickname = playerSelect.options[playerSelect.value].text;
+        foreach (var player in players)
+        {
+            if (player.GetNickname().Equals(nickname))
+            {
+                return player;
+            }
+        }
+        return null;
     }
 
     public void UpdateBoins()
     {
-        boins.text = player.GetBoins().ToString();
+        boins.text = activePlayer.GetBoins().ToString();
+    }
+
+    public Player GetActivePlayer()
+    {
+        return activePlayer;
     }
 }
