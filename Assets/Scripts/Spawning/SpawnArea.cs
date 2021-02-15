cc35d52793b6411b2f83fd98466e654d5d403ec5
feldtsen <feldtsen@gmail.com>
2021-02-15 12:06:53 +0100

    Made the camera not be dependent of where you place it in the hierarchy.

--- Assets/Scripts/Spawning/SpawnArea.cs
+++ Assets/Scripts/Spawning/SpawnArea.cs
@@ -35,6 +35,7 @@ public class SpawnArea : MonoBehaviour
 
             holding.Add(currentEntity);
         }
+        
         while (holding.Count > gridWidth * gridWidth) {
             GameObject currentEntity = holding[0];
             Destroy(currentEntity);
@@ -42,6 +43,7 @@ public class SpawnArea : MonoBehaviour
         }
         //float unitWidth = entityToSpawn.GetComponent<Collider>().bounds.size.z;
         float unitWidth = 0.4f;
+
 
         float width = gridWidth * unitWidth;
         
