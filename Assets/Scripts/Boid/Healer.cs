10decf217c1e91c2d65638cd3a74fc07f5648f31
palmdrop <anton@exlex.se>
2021-03-04 19:46:59 +0100

    Balance healer

--- Assets/Scripts/Boid/Healer.cs
+++ Assets/Scripts/Boid/Healer.cs
@@ -7,20 +7,18 @@ using Random = UnityEngine.Random;
 
 public class Healer : Boid
 {
-
     private int _healAmount;
-    
-    private Shader _attackAnimationShader;
+    private static Shader _healAnimationShader;
 
     // Start is called before the first frame update
     void Start() {
         base.Start();
         
-        _attackAnimationShader = Shader.Find("Sprites/Default");
+        _healAnimationShader = Shader.Find("Sprites/Default");
 
-        cost = 10;
+        cost = 20;
         health = maxHealth = 100;
-        damage = 1;
+        damage = 0;
         maxSpeed = 4f;
         targetHeight = 2f;
         collisionAvoidanceDistance = 3f;
@@ -30,27 +28,27 @@ public class Healer : Boid
         emotionalState = 0f;
         morale = 1f;
         
-        timeBetweenActions = 0.5f;
-        _healAmount = 5;
+        timeBetweenActions = 0.2f;
+        _healAmount = 4;
         
         classInfo = new ClassInfo {
             viewRadius = 3f,
             separationRadius = 0.3f,
-            fearRadius = 1.3f,
+            fearRadius = 1.2f,
             maxForce = 2.0f,
             healRadius = 1f,
             
             alignmentStrength = 6.0f,
             alignmentExponent = 0.0f, 
             
-            cohesionStrength = 6.0f,
+            cohesionStrength = 10.0f,
             cohesionExponent = 0.0f,
             
-            separationStrength = 110.0f,
+            separationStrength = 120.0f,
             separationExponent = 1.0f,
             
-            fearStrength = 100.0f,
-            fearExponent = 0.5f,
+            fearStrength = 200.0f,
+            fearExponent = 1.5f,
             
             // Negative attack distance implies that healing distance should be used instead
             attackDistRange = -1f,
@@ -59,7 +57,7 @@ public class Healer : Boid
             approachMovementStrength = 35.0f,
             approachMovementExponent = 0.5f,
             
-            aggressionStrength = 10.4f,
+            aggressionStrength = 8.4f,
             
             randomMovements = 4.0f,
         };
@@ -93,10 +91,10 @@ public class Healer : Boid
 
     private void AnimateHeal(Vector3 fromPos, Vector3 toPos) {
         LineRenderer lineRenderer = new GameObject("Line").AddComponent<LineRenderer>();
-        lineRenderer.material = new Material(_attackAnimationShader);
+        lineRenderer.material = new Material(_healAnimationShader);
         lineRenderer.startWidth = 0.1f;
         lineRenderer.endWidth = 0.01f;
-        lineRenderer.startColor = Color.yellow;
+        lineRenderer.startColor = new Color(0.5f, 0.5f, 0.0f, 0.5f);
         lineRenderer.positionCount = 2;
         lineRenderer.useWorldSpace = true;
         lineRenderer.SetPosition(0, fromPos);
