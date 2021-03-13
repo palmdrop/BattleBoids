340f19a340b18f11260a5819b60f7a6a4b6b651b
Anton Ahl <ahanton@student.chalmers.se>
2021-03-13 12:06:24 +0100

    Add basic UI structure for options and credits menu

---
+++ Assets/Scripts/Menu/OptionsManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionsManager : MonoBehaviour
+{
+    public void SetMasterVolume() {
+
+    }
+
+    public void SetMusicVolume() {
+
+    }
+
+    public void SetSfxVolume() {
+
+    }
+}
